28ac0eceb965310aaa431e9f01b1d78aa46826a0
Mykhailo Lazebnyi <miskadl09@gmail.com>
2022-08-30 21:38:38 +0200

    format

--- lib/router/blaze_configuration.dart
+++ lib/router/blaze_configuration.dart
@@ -44,9 +44,9 @@ abstract class IBlazeConfiguration implements RouteInformation {
   });
 
   @override
-  String toString() => '$runtimeType(location: $location, ' 
-  'state: $state, '
-  'pathParams: $pathParams)';
+  String toString() => '$runtimeType(location: $location, '
+      'state: $state, '
+      'pathParams: $pathParams)';
 }
 
 abstract class BaseBlazeConfiguration extends IBlazeConfiguration {
