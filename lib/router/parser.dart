0407c6683a53850191b135d74823732a6bb35acb
Mykhailo Lazebnyi <miskadl09@gmail.com>
2022-09-02 23:46:20 +0200

    updated change log and parser code refactor

--- lib/router/parser.dart
+++ lib/router/parser.dart
@@ -24,44 +24,25 @@ class BlazeParser extends IBlazeParser {
       'ParseRouteInformation loc: ${routeInformation.location} '
       'state: ${routeInformation.state}',
     );
-    try {
-      final state = routeInformation.state as Map<String, dynamic>? ??
-          const <String, dynamic>{};
-      try {
-        final pathArgs = <String, String>{};
-        final uri = Uri.parse(
-          routeInformation.location ?? '',
-        );
-        final matchedRoutes = _matcher(
-          location: uri.toString(),
-          state: state,
-          pathArgs: pathArgs,
-        );
-        return SynchronousFuture(
-          BlazeConfiguration(
-            location: uri.toString(),
-            state: state,
-            mathedRoutes: matchedRoutes,
-            pathParams: pathArgs,
-          ),
-        );
-      } on Object catch (e) {
-        l('ParseRouteInformation error: $e');
-        return SynchronousFuture(
-          BlazeConfiguration(
-            location: routeInformation.location!,
-            state: state,
-          ),
-        );
-      }
-    } on Object catch (e) {
-      l('Failed to get state from routeInformation: $e');
-      return SynchronousFuture(
-        BlazeConfiguration(
-          location: routeInformation.location ?? '',
-        ),
-      );
-    }
+    final state = routeInformation.state as Map<String, dynamic>? ??
+        const <String, dynamic>{};
+    final pathArgs = <String, String>{};
+    final uri = Uri.parse(
+      routeInformation.location ?? '',
+    );
+    final matchedRoutes = _matcher(
+      location: uri.toString(),
+      state: state,
+      pathArgs: pathArgs,
+    );
+    return SynchronousFuture(
+      BlazeConfiguration(
+        location: uri.toString(),
+        state: state,
+        mathedRoutes: matchedRoutes,
+        pathParams: pathArgs,
+      ),
+    );
   }
 
   @override
